36ce4b057bc63f4ca27196eec479d1fb596ae893
Joaquin Bejar <jb@taunais.com>
2024-05-29 18:40:56 +0200

    fix save models in re-train

--- src/re-train.py
+++ src/re-train.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from marketmimic.constants import BATCH_SIZE, EPOCHS
@@ -46,44 +47,9 @@ if __name__ == '__main__':
                 end = time.time()
                 print(f"Time to train: {end - start:.2f}")
 
-# if __name__ == '__main__':
-#     # zip_file = '../data/AAPL-Tick-Standard.txt.zip'
-#     # txt_file = 'AAPL-Tick-Standard.txt'
-#     zip_file = '../data/AMD-Tick-Standard.txt.zip'
-#     txt_file = 'AMD-Tick-Standard.txt'
-#
-#     # Load data
-#     df = load_data(zip_file, txt_file)
-#     df = join_date_time(df, 'Date', 'Time')
-#     # df = generate_market_data_from_func(100)
-#
-#     generator, discriminator, gan, gen_optimizer, disc_optimizer = build_gan()
-#     checkpoint = tf.train.Checkpoint(generator_optimizer=gen_optimizer,
-#                                      discriminator_optimizer=disc_optimizer,
-#                                      generator=generator,
-#                                      discriminator=discriminator)
-#
-#     path = '../models/v0.1'
-#     manager = tf.train.CheckpointManager(checkpoint,
-#                                          directory=path,
-#                                          max_to_keep=5)
-#     checkpoint.restore(manager.latest_checkpoint)
-#     if manager.latest_checkpoint:
-#         print("Restaurado de {}".format(manager.latest_checkpoint))
-#     else:
-#         print("Inicializando desde cero.")
-#
-#     # Prepare data
-#     data_scaled, _ = prepare_data(df)
-#     start = time.time()
-#     # Train GAN
-#     train_gan(generator,
-#               discriminator,
-#               gen_optimizer,
-#               disc_optimizer,
-#               data_scaled,
-#               epochs=EPOCHS,
-#               batch_size=BATCH_SIZE,
-#               reset_weights=False)
-#     end = time.time()
-#     print(f"Time to train: {end - start:.2f}")
+                # Save models
+                checkpoint_prefix = os.path.join(path, "ckpt")
+                checkpoint.save(file_prefix=checkpoint_prefix)
+
+
+
